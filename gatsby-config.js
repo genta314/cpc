1222c93a762cb73f183d04f14796ebebcb0368d0
Genta TANAKA <genta314@users.noreply.github.com>
2021-02-13 02:19:02 +0900

    Change Blog Settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Cynical Penguin Cafe`,
   },
   plugins: [
     {
@@ -25,12 +25,12 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `Fediverse`,
+            url: `https://curenoize.org`,
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `Diary`,
+            url: `https://write.curenoize.org`,
           },
         ],
       },
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: process.env.UA-118481384-1,
       },
     },
     `gatsby-plugin-sitemap`,
